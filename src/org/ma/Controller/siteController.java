1532f7567fbf96ae126885084278a5318c7e6cfe
tonmoysaha <tonmoysahaopi@gmail.com>
2019-05-30 12:21:03 +0600

    add login functionality

--- src/org/ma/Controller/siteController.java
+++ src/org/ma/Controller/siteController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import javax.sql.DataSource;
 
 import org.ma.entity.User;
+import org.ma.model.userDAO;
 import org.ma.model.userModel;
 
 /**
@@ -21,46 +22,67 @@ import org.ma.model.userModel;
 @WebServlet("/siteController")
 public class siteController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	@Resource(name="jdbc/project")
+	@Resource(name = "jdbc/project")
 	private DataSource datasource;
-   
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-	
-		
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	String action= request.getParameter("action");
-	switch (action) {
-	case "userregistration":
-		userRegistration(request,response);
-		break;
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getParameter("action");
+		switch (action) {
+		case "login":
+			userLogin(request, response);
+			break;
+		case "userregistration":
+			userRegistration(request, response);
+			break;
 
-	default:
-		break;
-	}
+		default:
+			break;
+		}
 	}
 
-	private void userRegistration(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		User newuser= new User(request.getParameter("fristname"), request.getParameter("lastname"), request.getParameter("email"), request.getParameter("username"), request.getParameter("password"));
+	private void userRegistration(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		User newuser = new User(request.getParameter("fristname"), request.getParameter("lastname"),
+				request.getParameter("email"), request.getParameter("username"), request.getParameter("password"));
 		addnewUser(newuser);
 		request.setAttribute("reg", "registration sucerss");
 		request.getRequestDispatcher("/login.jsp").forward(request, response);
-		
+
 	}
 
 	private void addnewUser(User newuser) {
-		new userModel().addNewuser(datasource,newuser);
-   		return;
+		new userModel().addNewuser(datasource, newuser);
+		return;
 	}
 
-	
+	private void userLogin(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("uname");
+		String password = request.getParameter("pass");
+		User login = new User(username, password);
+		login = userDAO.userLogin(datasource, login);
+		if (login.isValid()) {
+			request.getSession().invalidate();
+			HttpSession newsession = request.getSession(true);
+			newsession.setAttribute("user", username);
+			newsession.setMaxInactiveInterval(300);
+			response.sendRedirect("home.jsp");
 
+		} else {
+			response.sendRedirect("login.jsp");
+		}
 
-	
+	}
 
 }
---
+++ src/org/ma/filter/userControllerfilter.java
@@ -0,0 +1,56 @@
+package org.ma.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet Filter implementation class userControllerfilter
+ */
+@WebFilter("/userController")
+public class userControllerfilter implements Filter {
+
+	/**
+	 * Default constructor.
+	 */
+	public userControllerfilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		if (request.getSession().getAttribute("user") == null) {
+			request.getRequestDispatcher("login.jsp").forward(request, response);
+		} else {
+			chain.doFilter(request, response);
+		}
+
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
